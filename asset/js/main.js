2a4e6e75647ee296cbf0b470af22494db2b2ccf4
andrea1632 <andrea.bellucci10@libero.it>
2022-03-30 15:07:26 +0200

    esercizio completo

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data:{
+            message: 'Hello World!',
+            imgSrc: 'https://cloudfront-us-east-2.images.arcpublishing.com/reuters/TP2HFJ63ARPQTHFYUYFAAQZXWQ.jpg'
+        }
+    }
+)
